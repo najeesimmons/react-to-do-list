cabc7f99c793dcdd217c1e8ad9f5c70c9b882edb
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-28 16:24:06 -0400

    create state for todos and functions that change it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
-import './App.css';
-import TodoForm from './components/TodoForm';
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addToDo = (text) => {
+    let id = 1;
+    if (todos.length > 0) {
+      id = todos[0].id + 1;
+    }
+    let todo = { id: id, text: text, completed: false };
+    let newtodos = [todo, ...todos]
+    console.log(newtodos)
+    // this syntax adds newly created todo to the BEGINNING 
+    // of the todos state array
+    setTodos([todo, ...todos])
+  };
   return (
-    <div className='to-do-app' >
+    <div className="to-do-app">
       <h1>To Do List</h1>
-      <TodoForm />
+      <TodoForm addToDo={addToDo} />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export default function TodoForm() {
-  const [input, setInput] = useState("")
+export default function TodoForm(props) {
+  const [input, setInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addToDo(input)
+    setInput("")
+  }
+
   return (
-    <form className='to-do-form'>
-        <input onChange={(e) => setInput(e.target.value)} className='to-do-input' placeholder='Add a To-Do'/>
-        <button className='to-do-button'>Add To Do</button>
+    <form onSubmit={handleSubmit} className="to-do-form">
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        className="to-do-input"
+        placeholder="Add a To-Do"
+      />
+      <button type="submit" className="to-do-button">Add To Do</button>
     </form>
-  )
+  );
 }
